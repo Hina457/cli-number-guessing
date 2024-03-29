6aca6f8c4c118c257509b2e724b09aac3481fc70
Hina Alvi <princess4177812@gmail.com>
2024-03-29 00:55:10 -0700

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() *5+ 1);
+
+const answers = await inquirer.prompt([
+    
+ {
+
+    name:"userGuessedNumber",
+    type:"number",
+    message:"please guess a number between 1 - 5: ",
+ },
+]);
+
+ 
+if(answers.userGuessedNumber === randomnumber){
+   console.log("Congratulation! you guessed right number.");   
+}
+else{
+   console.log("you guessed wrong number ");
+   console.log("Thank you!");
+   
+};
